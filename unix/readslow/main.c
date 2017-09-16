a39fc5d99b3fd7512ce7c7d94197032aede03cb4
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-16 19:03:38 +0530

    Solved 7-3 - Add option -e to seek to end of file

--- unix/readslow/main.c
+++ unix/readslow/main.c
@@ -1,18 +1,43 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <fcntl.h>
 #define SIZE 256
 
 int main(int argc, char **argv) {
     char buf[SIZE];
-    int n;
+    int n, c;
     int sleep_time = 3;
+    bool seek_to_end = false;
+    int fd;
 
-    if (argc > 1 && argv[1][0] == '-') {
-        sleep_time = atoi(argv[1] + 1);
+    while ((c = getopt(argc, argv, "en:")) != -1) {
+        switch (c) {
+        case 'e':
+            seek_to_end = true;
+            break;
+        case 'n':
+            sleep_time = atoi(optarg);
+            break;
+        }
+    }
+
+    if (optind < argc) {
+        fd = open(argv[optind], 0);
+        if (fd == -1) {
+            perror(argv[optind]);
+            exit(1);
+        }
+
+        if (seek_to_end) {
+            lseek(fd, 0, SEEK_END);
+        }
     }
 
     for (;;) {
-        while ((n = read(0, buf, sizeof buf)) > 0) {
+        while ((n = read(fd, buf, sizeof buf)) > 0) {
             write(1, buf, n);
         }
         sleep(sleep_time);
