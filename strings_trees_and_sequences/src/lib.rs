5331322db3cbf2ac8082d433e77661cb6930fb33
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-11-05 15:46:58 +0530

    Solved exercise 1.1 - rotation detection assuming linear search is available

--- strings_trees_and_sequences/src/lib.rs
+++ strings_trees_and_sequences/src/lib.rs
@@ -1,3 +1,16 @@
+pub fn contains(_text: &str, _pattern: &str) -> bool {
+    //TODO
+    true
+}
+
+pub fn is_rotation_of<S: AsRef<str>>(a: S, b: S) -> bool {
+    if a.as_ref().len() != b.as_ref().len() {
+        return false;
+    }
+    let double = b.as_ref().to_owned() + b.as_ref();
+    return contains(&double, a.as_ref());
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
