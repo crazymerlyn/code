52b210d1cb8018063ec18edc418175f114d40c96
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-11-05 15:52:05 +0530

    Solved exercise 1.2 - substring of circular string

--- strings_trees_and_sequences/src/lib.rs
+++ strings_trees_and_sequences/src/lib.rs
@@ -11,6 +11,14 @@ pub fn is_rotation_of<S: AsRef<str>>(a: S, b: S) -> bool {
     return contains(&double, a.as_ref());
 }
 
+pub fn is_contained_in_rotation_of<S: AsRef<str>>(a: S, b: S) -> bool {
+    if a.as_ref().len() > b.as_ref().len() {
+        return false;
+    }
+    let double = b.as_ref().to_owned() + b.as_ref();
+    return contains(&double, a.as_ref())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
