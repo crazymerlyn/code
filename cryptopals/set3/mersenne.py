c0a2dc754415625049dc50cf581dde90eee390f8
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-03-22 17:56:19 +0530

    Solved cryptopals challenge 21 - Implement Mersenne twister

--- cryptopals/set3/mersenne.py
+++ cryptopals/set3/mersenne.py
@@ -23,5 +23,44 @@ class Mersenne(object):
             self.mt[i] = ((((s & 0xffff0000) >> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253 + self.mti
             self.mt[i] >>= 0
 
+    def genrand_int32(self):
+        mag01 = [0, self.matrix_a]
+        if self.mti >= self.N:
+            if self.mti == self.N + 1:
+                self.init_genrand(5489)
 
+            for k in range(self.N - self.M):
+                y = (self.mt[k] & self.upper_mask) | (self.mt[k+1] & self.lower_mask)
+                self.mt[k] = self.mt[k + self.M] ^ (y >> 1) ^ mag01[y & 1]
+
+            for k in range(self.N - self.M, self.N - 1):
+                y = (self.mt[k] & self.upper_mask) | (self.mt[k+1] & self.lower_mask)
+                self.mt[k] = self.mt[k + self.M - self.N] ^ (y >> 1) ^ mag01[y & 1]
+
+            y = (self.mt[self.N - 1] & self.upper_mask) | (self.mt[0] & self.lower_mask)
+            self.mt[self.N - 1] = self.mt[self.M - 1] ^ (y >> 1) ^ mag01[y & 1]
+            for i in range(len(self.mt)):
+                self.mt[i] &= 0xffffffff
+            self.mti = 0
+        y = self.mt[self.mti]
+        self.mti += 1
+        y ^= (y >> 11)
+        y ^= (y << 7) & 0x9d2c5680
+        y ^= (y << 15) & 0xefc60000
+        y ^= (y >> 18)
+
+        return y
+
+
+
+if __name__ == '__main__':
+    a = Mersenne()
+    n = 10 ** 5
+    count = 0
+    for _ in range(n):
+        x = a.genrand_int32() / 2.0 ** 32
+        y = a.genrand_int32() / 2.0 ** 32
+        if x ** 2 + y ** 2 <= 1:
+            count += 1
+    print(4.0 * count/n)
 
