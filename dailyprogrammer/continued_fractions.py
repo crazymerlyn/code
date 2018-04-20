5d72d144d5e7700655542a56301c7711ee1e588b
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2018-04-21 05:14:49 +0530

    Implement mathml rendering for continued fractions

--- dailyprogrammer/continued_fractions.py
+++ dailyprogrammer/continued_fractions.py
@@ -20,7 +20,12 @@ def continued_to_latex(gauss):
     if len(gauss) == 1: return str(gauss[0])
     return "%d + \cfrac{1}{%s}" % (gauss[0], continued_to_latex(gauss[1:]))
 
+def continued_to_mathml(gauss):
+    if len(gauss) == 1: return "<mn>%d</mn>" % gauss[0]
+    return "<mrow><mn>%d</mn><mo>+</mo><mfrac><mn>1</mn>%s</mfrac></mrow>" % (gauss[0], continued_to_mathml(gauss[1:]))
+
 print(improper_to_continued(Fraction(45, 16)))
 print(continued_to_improper([2, 1, 7]))
 print(continued_to_improper([2, 2, 1, 1]))
 print(continued_to_latex([2, 2, 1, 1]))
+print(continued_to_mathml([2, 2, 1, 1]))
